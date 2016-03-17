fdf1e658456da6b65a039c3615124f7d14e9c2b0
MOSapeizer <k471352@gmail.com>
2016-03-17 14:22:35 +0800

    Remove GetObservation xml

--- lib/sos-helper.rb
+++ lib/sos-helper.rb
@@ -7,15 +7,27 @@ Dir[ File.dirname(__FILE__) + "/sos_helper/*.rb"].each {|file| require file }
 
 module SOSHelper
 	Dir[ File.dirname(__FILE__) + "/sos_helper/*.rb"]
-	Urls = ["http://cgis.csrsr.ncu.edu.tw:8080/swcb-sos-new/service",
-			  "http://cgis.csrsr.ncu.edu.tw:8080/epa-sos/service",
-			  "http://cgis.csrsr.ncu.edu.tw:8080/epa-aqx-sos/service"].freeze
+	Urls = [ "http://cgis.csrsr.ncu.edu.tw:8080/swcb-sos-new/service",
+			 "http://cgis.csrsr.ncu.edu.tw:8080/epa-sos/service",
+			 "http://cgis.csrsr.ncu.edu.tw:8080/epa-aqx-sos/service"].freeze
 
-	Relics = ["offering","observedProperty",
-	     "featureOfInterest",
-		 "procedure", "spatialFilter", 
-		 "temporalFilter", "responseFormat"].freeze
+	Relics = [ "offering","observedProperty",
+		       "featureOfInterest",
+			   "procedure", "spatialFilter", 
+			   "temporalFilter", "responseFormat"].freeze
 
-	ObservationRequest = File.open('./lib/sos_helper/getObservation.xml') { |f| Nokogiri::XML(f)  }.freeze
+	ObservationRequestXML = <<-REQUEST
+							<?xml version="1.0" encoding="UTF-8"?>
+							<sos:GetObservation
+						    xmlns:sos="http://www.opengis.net/sos/2.0"
+						    xmlns:fes="http://www.opengis.net/fes/2.0"
+						    xmlns:gml="http://www.opengis.net/gml/3.2"
+						    xmlns:swe="http://www.opengis.net/swe/2.0"
+						    xmlns:xlink="http://www.w3.org/1999/xlink"
+						    xmlns:swes="http://www.opengis.net/swes/2.0"
+						    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" service="SOS" version="2.0.0" xsi:schemaLocation="http://www.opengis.net/sos/2.0 http://schemas.opengis.net/sos/2.0/sos.xsd">\n  </sos:GetObservation>
+						    REQUEST
+
+    ObservationRequest = Nokogiri::XML(ObservationRequestXML)
 
 end
